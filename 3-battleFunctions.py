baac6616a7349d2e319d866207a47cf5869a6133
Leo Zhi <leozhi6869@gmail.com>
2023-03-04 10:06:36 -0800

    battle

--- 3-battleFunctions.py
+++ 3-battleFunctions.py
@@ -1,2 +1,7 @@
-def battle(bossDmg,bossHlth,heroDmg,heroHlth,heroDef):
+#import modules
+import random
+
+def battle(bossHlth,heroDmg,heroHlth,heroDef):
+    # choose random boss damage
+    bossDmg = random.choice(range(50,100))
     
