4dbff6bfc289ecf9ec88ead8cb3974bcc06d9bd9
Jacobmar2 <72660157+Jacobmar2@users.noreply.github.com>
2023-03-04 12:01:36 -0800

    added choices

--- components/hero.py
+++ components/hero.py
@@ -7,3 +7,52 @@ class hero:
 
     def update_health(self, new_health):
         self.health = new_health
+
+    def start(self):
+        self.health = 500
+        self.dmg = 50
+        self.defence = 0
+        self.dodge = 0
+
+    def decision(self, choice):
+        # choice is boolean, yes is right, no is left
+        if choice:
+            self.defence += 30
+        else:
+            self.dmg += 80
+
+    def decision2(self, choice2):
+        if self.choice: #right side
+            if self.choice2:
+                self.dodge += 50
+            else:
+                self.dmg += 50
+        else:
+            if choice2:
+                self.dodge += 30
+            else:
+                self.dmg += 100
+
+    def decision3(self, choice3):
+        if self.choice:
+            if self.choice2:
+                if self.choice3:
+                    self.dodge += 40
+                else:
+                    self.dmg += 150
+            else:
+                if self.choice3:
+                    self.dodge += 40
+                else:
+                    self.defence += 70
+        else:
+            if self.choice2:
+                if self.choice3:
+                    self.defence += 40
+                else:
+                    self.dmg += 200
+            else:
+                if self.choice3:
+                    self.defence += 40
+                else:
+                    self.dmg += 269
