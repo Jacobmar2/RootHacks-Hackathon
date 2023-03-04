9288a6e222bbfe93ffb161dc8529ab77b94696fd
Jacobmar2 <72660157+Jacobmar2@users.noreply.github.com>
2023-03-04 12:16:10 -0800

    Update hero.py

--- components/hero.py
+++ components/hero.py
@@ -22,6 +22,7 @@ class hero:
 
     def decision(self, choice):
         # choice is boolean, yes is right, no is left
+        print("Make your choice. Left button for +80 damage, Right button for +30 defence")
         if choice:
             self.defence += 30
         else:
@@ -29,11 +30,13 @@ class hero:
 
     def decision2(self, choice2):
         if self.choice:  # right side
+            print("Make your choice. Left button for +50 damage, Right button for +50 dodge")
             if self.choice2:
                 self.dodge += 50
             else:
                 self.dmg += 50
         else:
+            print("Make your choice. Left button for +100 damage, Right button for +30 dodge")
             if choice2:
                 self.dodge += 30
             else:
@@ -42,22 +45,26 @@ class hero:
     def decision3(self, choice3):
         if self.choice:
             if self.choice2:
+                print("Make your choice. Left button for +150 damage, Right button for +40 dodge")
                 if self.choice3:
                     self.dodge += 40
                 else:
                     self.dmg += 150
             else:
+                print("Make your choice. Left button for +70 defence, Right button for +40 dodge")
                 if self.choice3:
                     self.dodge += 40
                 else:
                     self.defence += 70
         else:
             if self.choice2:
+                print("Make your choice. Left button for +200 damage, Right button for +40 defence")
                 if self.choice3:
                     self.defence += 40
                 else:
                     self.dmg += 200
             else:
+                print("Make your choice. Left button for +269 damage, Right button for +40 defence")
                 if self.choice3:
                     self.defence += 40
                 else:
