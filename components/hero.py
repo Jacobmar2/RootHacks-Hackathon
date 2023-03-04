13b992ebc8e7768811b6198ab56cc66a7c7a1da9
Leo Zhi <leozhi6869@gmail.com>
2023-03-04 15:07:42 -0800

    add healthbar class

--- components/hero.py
+++ components/hero.py
@@ -6,6 +6,8 @@ class Hero:
         self.dodge = dodge
 
     def uHealth(self, new_health):
+        if new_health < 0:
+            self.health = 0
         self.health = new_health
 
     def uDmg(self, new_damage):
@@ -22,3 +24,19 @@ class Hero:
         print("Damage = ", self.dmg)
         print("Defence = ", self.defence)
         print("Dodge = ", self.dodge)
+
+
+    # Nested Health Bar Class
+    class Healthbar(pygame.sprite, Sprite):
+        def __init__(self):
+            super().__init__()
+            self.image = pygame.Surface((400, 400))
+            self.image.fill((240, 240, 240))
+            self.rect = self.image.get_rect(centre=(400, 400))
+            self.current_health =
+            self.maximum_health = 500
+            self.health_bar_length = 250
+            self.health_ratio = self.maximum_health / self.health_bar_length
+
+        def update(self):
+            pass
