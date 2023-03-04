f6210dac2c07fd3315286176a264730378fc6228
Peter Soava <pgs2@sfu.ca>
2023-03-04 15:53:13 -0800

    Adjust plantguy size

--- states/battleScreen.py
+++ states/battleScreen.py
@@ -6,7 +6,7 @@ class Battle(BaseState):
         super(Battle, self).__init__()
         self.rect = pygame.Rect((0, 0), (80, 80))
         self.rect.center = self.screen_rect.center
-        self.character = pygame.image.load('images/plantguy.png').convert()
+        self.character = pygame.image.load('images/plantguy2.png')
         self.character_spawn = self.character.get_rect(center=self.screen_rect.center)
         self.x = 0
         self.y = 0
@@ -30,6 +30,7 @@ class Battle(BaseState):
                 self.done = True
 
     def update(self, dt):
+        pygame.display.get_surface().fill(pygame.Color("black"))
         self.o.move()
         pygame.display.get_surface().blit(self.o.image, self.o.pos)
         pygame.display.update()
