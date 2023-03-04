82954bf8ca512a09a47b451443873a0dfb353d4a
Leo Zhi <leozhi6869@gmail.com>
2023-03-04 09:50:01 -0800

    healthbar

--- 3-healthbarFunctions.py
+++ 3-healthbarFunctions.py
@@ -1,4 +1,6 @@
+# boss health should return an int for the new boss health
 def bossHealth(oldHealth, newHealth, damage):
 
 
+# hero health should return an int for the new hero
 def heroHealth(oldHealth, newHealth, damage):
