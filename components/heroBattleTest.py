230e4b81224527e53b2876c8f3f6cd1b63917d43
Jacobmar2 <72660157+Jacobmar2@users.noreply.github.com>
2023-03-04 18:03:21 -0800

    completed decision binary tree

--- components/heroBattleTest.py
+++ components/heroBattleTest.py
@@ -1,21 +1,32 @@
-import boss, hero, battleFunctions, evolutions
+import boss, hero, battleFunctions, evolutions, displayTree
 
 # initialize hero
 hero_inst = hero.Hero(500, 50, 0, 0)
+displayTree.show_tree()
 hero_inst.print_stats()
 print("================")
+
+
 
 # first evolution
 hero_inst = evolutions.upgrade(1)
+displayTree.choice1(1)
+displayTree.show_tree()
 hero_inst.print_stats()
 print("================")
+
 
 # second evolution
 hero_inst = evolutions.upgrade(1)
+displayTree.choice2(1)
+displayTree.show_tree()
 hero_inst.print_stats()
 print("================")
 
+
 # third evolution
 hero_inst = evolutions.upgrade(1)
+displayTree.choice3(1)
+displayTree.show_tree()
 hero_inst.print_stats()
 print("================")
